1f917402351b2c5ff7cdd3eda218bba69e4624bf
root <thiago.kraetzer@brainweb.com.br>
2021-05-04 22:50:32 -0300

    Finish: finishing refactoring

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from 'react';
+
+import {Food as FoodInterface} from '../../types/types';
+
+import Header from '../../components/Header';
+import api from '../../services/api';
+import Food from '../../components/Food';
+import ModalAddFood from '../../components/ModalAddFood';
+import ModalEditFood from '../../components/ModalEditFood';
+
+
+import { FoodsContainer } from './styles';
+
+
+
+const Dashboard: React.FC = (props) => {
+  const [foods, setFoods] = useState<FoodInterface[]>([]); 
+  const [editingFood, setEditingFood] = useState<FoodInterface>({} as FoodInterface); 
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  const handleAddFood = async (food: any) => {
+    try {
+      const response = await api.post('/foods', {
+        ...food,
+        available: true,
+      });
+
+      setFoods([...foods, response.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleUpdateFood = async (food: any) => {
+    try {
+      const foodUpdated = await api.put(
+        `/foods/${editingFood.id}`,
+        { ...editingFood, ...food },
+      );
+
+      const foodsUpdated = foods.map(f =>
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data,
+      );
+
+      setFoods(foodsUpdated);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleDeleteFood = async (id: number) => {
+    await api.delete(`/foods/${id}`);
+
+    const foodsFiltered = foods.filter(food => food.id !== id);
+
+    setFoods(foodsFiltered);
+  }
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  }
+
+  const toggleEditModal = () => {
+    setEditModalOpen(!editModalOpen);
+  }
+
+  const handleEditFood = (food: any) => {
+    setEditingFood(food);
+    setEditModalOpen(true);
+  }
+
+  useEffect(() => {
+    const getFoods = async () => {
+      const response = await api.get('/foods');
+
+      setFoods(response.data);
+    }
+    getFoods();
+  }, []);
+
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map(food => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+}
+
+export default Dashboard;
